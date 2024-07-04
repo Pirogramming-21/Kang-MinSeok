e59b7c71bca13361be5979ba5e4fc34ac77c841c
minseok kang <alstjrrkd201@gmail.com>
2024-07-05 02:14:02 +0900

    2단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,6 +1,11 @@
+import random
+
 class Player:
     def __init__(self):
         self.name = self.__class__.__name__
+
+    print("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ", end= ' ')
+    count_input = input()
 
 
 class Computer:
