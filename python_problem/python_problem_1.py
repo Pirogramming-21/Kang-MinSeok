b843a0b07672d47df086909138f8b4490ef96a68
minseok kang <alstjrrkd201@gmail.com>
2024-07-05 02:14:46 +0900

    3단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -4,8 +4,19 @@ class Player:
     def __init__(self):
         self.name = self.__class__.__name__
 
-    print("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ", end= ' ')
-    count_input = input()
+
+    def brGame(self, num, opponent):
+        while True:
+            print("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ", end= ' ')
+            count_input = input()
+            if count_input.isdigit():  # 입력이 숫자인지 확인
+                count = int(count_input)
+                if 1 <= count <= 3:
+                    break
+                else:
+                    print("1, 2, 3 중 하나를 입력하세요")
+            else:
+                print("정수를 입력하세요")
 
 
 class Computer:
