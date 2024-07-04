c9d94534a8bc6ea5a5527b74da735fd923b92361
minseok kang <alstjrrkd201@gmail.com>
2024-07-05 02:16:24 +0900

    9단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -9,7 +9,7 @@ class Player:
         while True:
             print("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ", end= ' ')
             count_input = input()
-            if count_input.isdigit():  # 입력이 숫자인지 확인
+            if count_input.isdigit(): 
                 count = int(count_input)
                 if 1 <= count <= 3:
                     break
@@ -21,14 +21,35 @@ class Player:
         for i in range(count):
             num += 1
             print(f"{self.name} {num}")
-
+            if num == 31:
+                print(f"{opponent.name} win!")
+                return num, True
+        return num, False
 
 class Computer:
     def __init__(self):
         self.name = self.__class__.__name__
 
+
+    def brGame(self, num, opponent):
+        count = random.randint(1, 3)
+
+        for i in range(count):
+            num += 1
+            print(f"{self.name} {num}")
+            if num == 31:
+                print(f"{opponent.name} win!")
+                return num, True
+        return num, False
+
 player = Player()
 computer = Computer()
 
 num = 0
+finish = False
 
+while not finish:
+    num, finish = computer.brGame(num, player)
+    if finish:
+        break
+    num, finish = player.brGame(num, computer)
