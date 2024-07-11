e875e0d3f3aabdaf14c6c44d2249333f91887063
minseok kang <alstjrrkd201@gmail.com>
2024-07-12 01:28:03 +0900

    실습

---
+++ 실습/3주차/CRUD 기초/posts/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    title=models.CharField(max_length=32)
+    user=models.CharField(max_length=20)
+    content=models.TextField()
+
+class Comment(models.Model):
+    post=models.ForeignKey(Post, on_delete=models.CASCADE)
+    content=models.TextField()
